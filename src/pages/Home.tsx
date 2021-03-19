ef2e1a00998ef4620aeafd2ff00192e7eac83fd9
Gabriel Germano <gabriel.lucasgermano@gmail.com>
2021-03-19 17:44:55 -0300

    First Commit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,56 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, 
+         IonHeader, 
+         IonPage, 
+         IonTitle, 
+         IonToolbar, 
+         IonItem, 
+         IonLabel, 
+         IonToggle } 
+         from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
+import React, { useState } from 'react';
 import './Home.css';
 
 const Home: React.FC = () => {
+  const [checked, setChecked] = useState(false);
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>SmartHouse</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
+      <IonContent fullscreen scrollEvents={true}>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
+            <IonTitle size="large">IFAL</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer />
+        <br></br>
+        <IonLabel> Luzes da casa</IonLabel>
+        <IonItem>
+          <IonLabel>Led Piscina </IonLabel>
+          <IonToggle color="primary" />
+        </IonItem>
+        <IonItem>
+          <IonLabel>Led Sala </IonLabel>
+          <IonToggle color="primary" />
+        </IonItem>
+        <IonItem>
+          <IonLabel>LDR </IonLabel>
+          <IonToggle color="primary" />
+        </IonItem>
+        <br></br>
+        <br></br>
+        <IonLabel>Outros</IonLabel>
+        <IonItem>
+          <IonLabel>Sirene</IonLabel>
+          <IonToggle color="primary" />
+        </IonItem>
+        <IonItem>
+          <IonLabel>Alarme</IonLabel>
+          <IonToggle color="primary" />
+          </IonItem>
       </IonContent>
     </IonPage>
   );
